8d003180096a155fd0fa85d0d8d2ba79d9dfca8c
joeblank <joe.blank@live.com>
2017-08-10 12:19:14 -0600

    unauthorized error notes

---
+++ auth/unauthorized-error.js
@@ -0,0 +1,4 @@
+// When getting an unauthorized error while you are trying to log in using Auth0:
+
+// 1. Verify that domain, clientID, clientSecret, and callbackURL are set up correctly in your auth strategy.
+// 2. Log in to auth0.com and check the rules tab. By default, you should not have any rules. If you do, remove/disable all rules.
